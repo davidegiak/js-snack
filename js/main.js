96b43b4a9c57779d2050a979593720dbe00b4b5e
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-26 21:31:57 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const one=prompt ("inserisci un numero");
+const two = prompt ("inserisci un numero");
+const thre = prompt ("inserisci un numero");
+const four = prompt ("inserisci un numero");
+const five = prompt ("inserisci un numero");
+const six = prompt ("inserisci un numero");
+const seven = prompt ("inserisci un numero");
+const eight = prompt ("inserisci un numero");
+const nine = prompt ("inserisci un numero");
+const ten = prompt ("inserisci un numero");
+
+let results = (Number(one) + Number(two) + Number(thre) + Number(four) + Number(five) + Number(six) + Number(seven) + Number(eight) + Number(nine) + Number(ten));
+
+console.log(results);
+
+document.getElementById("title").innerHTML=(`La somma è:` + results)
