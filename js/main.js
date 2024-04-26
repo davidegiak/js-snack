23617b694e30b6fe677e47bdffd5704dfdaea632
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-26 21:38:53 +0200

    primo esercizio

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
-const one=prompt ("inserisci un numero");
-const two = prompt ("inserisci un numero");
-const thre = prompt ("inserisci un numero");
-const four = prompt ("inserisci un numero");
-const five = prompt ("inserisci un numero");
-const six = prompt ("inserisci un numero");
-const seven = prompt ("inserisci un numero");
-const eight = prompt ("inserisci un numero");
-const nine = prompt ("inserisci un numero");
-const ten = prompt ("inserisci un numero");
+const primo = prompt("scrivi qualcosa");
+const secondo = prompt("scrivi qualcosa");
 
-let results = (Number(one) + Number(two) + Number(thre) + Number(four) + Number(five) + Number(six) + Number(seven) + Number(eight) + Number(nine) + Number(ten));
+if (primo.length > secondo.length){
+    console.log(primo)
+    console.log(secondo)
+}
+else {
+    console.log(secondo)
+    console.log(primo)
+}
 
-console.log(results);
 
-document.getElementById("title").innerHTML=(`La somma è:` + results)
+
+
+
+
+// *************************************************ESERCIZIO 2**********************************************************************
+// const one=prompt ("inserisci un numero");
+// const two = prompt ("inserisci un numero");
+// const thre = prompt ("inserisci un numero");
+// const four = prompt ("inserisci un numero");
+// const five = prompt ("inserisci un numero");
+// const six = prompt ("inserisci un numero");
+// const seven = prompt ("inserisci un numero");
+// const eight = prompt ("inserisci un numero");
+// const nine = prompt ("inserisci un numero");
+// const ten = prompt ("inserisci un numero");
+
+// let results = (Number(one) + Number(two) + Number(thre) + Number(four) + Number(five) + Number(six) + Number(seven) + Number(eight) + Number(nine) + Number(ten));
+
+// console.log(results);
+
+// document.getElementById("title").innerHTML=(`La somma è:` + results)
